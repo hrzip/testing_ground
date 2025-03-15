71278fc48d8ed1373a21c4aa1ae05c2b7edac9ea
hrzip <63956767+hrzip@users.noreply.github.com>
2025-03-15 22:07:54 +0100

    Riješen zadatak sa pdv-om

--- vježba_za_2f format.py
+++ vježba_za_2f format.py
@@ -4,10 +4,14 @@ print("Prva cijena: {:.2f} kn, Druga cijena: {:.2f} kn".format(cijena1, cijena2)
 # Ispisuje: Prva cijena: 10.50 kn, Druga cijena: 5.75 kn
 
 
+
+
 cijena1 = 10.5
 cijena2 = 5.75
 print("Prva cijena: {:.2f} kn, Druga cijena: {:.2f} kn".format(cijena1, cijena2))
 # Ispisuje: Prva cijena: 10.50 kn, Druga cijena: 5.75 kn
+
+
 
 
 cijena = 10.5
@@ -16,10 +20,19 @@ konacna_cijena = cijena * (1 - popust)
 print("Originalna cijena: {0:.2f} kn, Popust: {1:.2f}%, Konačna cijena: {2:.2f} kn".format(cijena, popust * 100, konacna_cijena))
 # Ispisuje: Originalna cijena: 10.50 kn, Popust: 20.00%, Konačna cijena: 8.40 kn
 
+'''`{0:.2f}` znači "uzmi prvi argument (indeks 0) iz `.format()` i formatiraj ga kao float s 2 decimale" - to je `cijena`
+`{1:.2f}` znači "uzmi drugi argument (indeks 1) iz `.format()` i formatiraj ga kao float s 2 decimale" - to je `popust * 100`
+`{2:.2f}` znači "uzmi treći argument (indeks 2) iz `.format()` i formatiraj ga kao float s 2 decimale" - to je `konacna_cijena`'''
+
+
+
 
 
 print("Rezultat: {broj:.2f}".format(broj=123.456789))
 # Ispisuje: Rezultat: 123.46
+
+
+
 
 
 postotak = 0.12345
@@ -40,6 +53,12 @@ a zatim ispiše cijenu s PDV-om (25%) formatiranu na dvije decimale.
 # Početak koda
 cijena_bez_pdv = float(input("Unesite cijenu bez PDV-a: "))
 # Dovršite kod...
+
+
+#Rješenje:
+cijena_bez_pdv = float(input("Unesite cijenu bez PDV-a: "))
+cijena_s_pdv = cijena_bez_pdv * 1.25  # Dodajemo 25% na osnovnu cijenu
+print('Cijena s PDV-om: {:.2f} kn'.format(cijena_s_pdv))
 
 
 
