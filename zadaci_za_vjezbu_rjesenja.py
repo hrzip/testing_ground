37c27b8e154f732ab516012e4e96b82df22e674f
hrzip <admin@hrzip.com>
2025-03-15 23:35:39 +0100

    Dodan četvrti zadatak

--- zadaci_za_vjezbu_rjesenja.py
+++ zadaci_za_vjezbu_rjesenja.py
@@ -1,3 +1,24 @@
+'''
+## Zadatak 4: Kalkulator uštede
+
+Napišite program koji traži od korisnika da unese mjesečni iznos koji planira štedjeti, godišnju kamatnu stopu i broj godina štednje. 
+Program treba izračunati i ispisati ukupni ušteđeni iznos nakon zadanog razdoblja, uzimajući u obzir složenu kamatu koja se obračunava mjesečno.
+'''
+
+mjesecni_iznos = float(input('Mjesečni iznos : '))
+kamata = float(input('Godišnja kamatna stopa : '))
+broj_godina = int(input('Broj godina : '))
+
+mjesecna_kamatna_stopa = kamata / 100 * 12
+
+ukupni_iznos = print('Ukupni ušteđeni iznos : ', mjesecni_iznos * kamata / broj_godina)
+
+
+
+
+
+
+
 ## Zadatak 1: Kalkulator potrošnje goriva
 
 '''Napišite program koji traži od korisnika da unese prijeđene kilometre i potrošeno gorivo u litrama. 
@@ -56,3 +77,23 @@ else:
     kategorija = "pretilost"
 
 print(f'Vaš BMI je {bmi:.1f}, što spada u kategoriju: {kategorija}')
+
+
+
+## Zadatak 4: Kalkulator uštede
+'''
+Napišite program koji traži od korisnika da unese mjesečni iznos koji planira štedjeti, godišnju kamatnu stopu i broj godina štednje. 
+Program treba izračunati i ispisati ukupni ušteđeni iznos nakon zadanog razdoblja, uzimajući u obzir složenu kamatu koja se obračunava mjesečno.
+'''
+
+mjesecni_iznos = float(input('Mjesečni iznos: '))
+kamata = float(input('Godišnja kamatna stopa: '))
+broj_godina = int(input('Broj godina: '))
+
+mjesecna_kamatna_stopa = kamata / 100 / 12
+
+ukupno_mjeseci = broj_godina * 12
+
+ukupni_iznos = mjesecni_iznos * ((1 + mjesecna_kamatna_stopa)**ukupno_mjeseci - 1) / mjesecna_kamatna_stopa * (1 + mjesecna_kamatna_stopa)
+
+print(f'Ukupni ušteđeni iznos: {ukupni_iznos:.2f}')
